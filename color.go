67fe449aed14fb9a14f7e78ec2157f55caf8410f
R Landau <rflandau@pm.me>
2025-01-24 16:01:51 -0800

    add pre-defined colors

---
+++ color.go
@@ -0,0 +1,13 @@
+package npxl
+
+import "image/color"
+
+var (
+	Red    = color.RGBA{R: 0xFF}
+	Orange = color.RGBA{R: 0xFF, G: 0xA5}
+	Yellow = color.RGBA{R: 0xFF, G: 0xFF}
+	Green  = color.RGBA{G: 0xFF}
+	Blue   = color.RGBA{B: 0xFF}
+	Navy   = color.RGBA{B: 0xA5}
+	Purple = color.RGBA{R: 0xFF, B: 0xFF}
+)
